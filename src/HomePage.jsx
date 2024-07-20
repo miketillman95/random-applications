ea970bf17f9687aabfd851311396f413ec769b16
Mike <matillman95@gmail.com>
2024-07-20 16:58:10 -0700

    linked page

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,36 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import './Css/HomePage.css'
 
 
 const HomePage = () => {
-const [count, setCount] = useState(0)
 
-const handleDecrement =() =>{
-    if(count <= 0) {
-        alert('Count cant go below 0')
-        setCount(0)
 
-    } else {
-        setCount(count -1)
-    }
-}
 
     return (
     <>
-        <h1 className="todo-title"> check out out the todo app</h1>
+        <h1 className="todo-title"> I am just making random react pages here for practice</h1>
         <div className="product-link">
-            <Link to='/todoApp'> Todo app</Link>
+            <Link to='/todoApp'> Todo app</Link> <br/>
+            <Link to='/counter'>Counter</Link>
         </div>
-        <br/>
-        <div className="state-counter">
-            <h1> Counter</h1>
-            <p>{count}</p>
-            <button onClick={() => setCount(count + 1)}> increase</button><br/>
-            <button onClick={() => handleDecrement()}> Decrease </button><br/>
-            <button onClick={() => setCount(0)}> Reset</button>
-        </div>
-        <br/>
         
         
     </>
---
+++ src/Pages/Counter.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+
+
+    const handleDecrement =() =>{
+        if(count <= 0) {
+            alert('Count cant go below 0')
+            setCount(0)
+    
+        } else {
+            setCount(count -1)
+        }
+    }
+
+  return (
+    <div className="state-counter">
+    <h1> Counter</h1>
+    <p>{count}</p>
+    <button onClick={() => setCount(count + 1)}> increase</button><br/>
+    <button onClick={() => handleDecrement()}> Decrease </button><br/>
+    <button onClick={() => setCount(0)}> Reset</button>
+</div>
+
+  )
+}
+
+export default Counter
+
