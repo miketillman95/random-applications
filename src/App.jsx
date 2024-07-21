d5de1efca47335ee88f198d6e8ef8e1d8c292925
Mike <matillman95@gmail.com>
2024-07-20 17:01:26 -0700

    route fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import '../src/Css/App.css'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import HomePage from './HomePage'
 import TodoApp from './Pages/TodoApp'
+import Counter from './Pages/Counter';
 
 const DATA = [
   { id: "todo-0", name: "Eat", completed: true },
@@ -14,6 +15,7 @@ function App() {
     <Routes>
         <Route index element={<HomePage />} />
         <Route path='todoApp' element={<TodoApp dataTasks= {DATA}/>} />
+        <Route path='counter' element={<Counter/>}/>
     </Routes>
   </Router>
   
--- src/Pages/Counter.jsx
+++ src/Pages/Counter.jsx
@@ -19,9 +19,11 @@ const Counter = () => {
     <div className="state-counter">
     <h1> Counter</h1>
     <p>{count}</p>
+    <ul>
     <button onClick={() => setCount(count + 1)}> increase</button><br/>
     <button onClick={() => handleDecrement()}> Decrease </button><br/>
     <button onClick={() => setCount(0)}> Reset</button>
+    </ul>
 </div>
 
   )
